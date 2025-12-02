3214d3980aa7b0f19f7f0b3a2624b9b2ba5169a9
SantoshHasabe <santoshhasabe567@gmail.com>
2025-12-02 22:15:05 +0530

    Added basic sequalise and cli related files

---
+++ src/routes/v2/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/info',(req, res) => {
+    return res.json({ msg: 'coming from v2'});
+})
+module.exports = router;
